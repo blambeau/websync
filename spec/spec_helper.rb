94f1fc67713a2035704e177888fe8676564a87c1
Bernard Lambeau <blambeau@gmail.com>
2011-10-14 18:26:13 +0200

    Extracted repo creation to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,47 @@ require 'fileutils'
 def fixtures_folder
   File.expand_path("../fixtures", __FILE__)
 end
+
+###
+
+def git_repo_base
+  File.join(fixtures_folder, "gitrepo")
+end
+
+def git_repo_origin
+  File.join(git_repo_base, "origin")
+end
+
+def git_repo_client
+  File.join(git_repo_base, "client")
+end
+
+###
+
+def build_git_repo
+  code = <<-EOF
+    rm -rf #{git_repo_base}
+    mkdir #{git_repo_base}
+    #
+    mkdir #{git_repo_origin}
+    cd #{git_repo_origin}
+    git init --bare
+    #
+    git clone -- #{git_repo_origin} #{git_repo_client}
+    cd #{git_repo_client}
+    echo "ignored file" > IGNORED.md
+    echo "initial commit" > README.md
+    echo "IGNORED.md" > .gitignore
+    git add README.md .gitignore
+    git commit -a -m "Initial repository layout"`
+  EOF
+  `#{code}`
+end
+
+def reset_client_repo
+  `cd #{git_repo_client}
+   rm -rf ADDED.md
+   git reset --hard`
+end
+
+
