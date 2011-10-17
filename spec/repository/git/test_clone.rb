e45004240ec346b00d2515f0becce6d3323b8d0a
Bernard Lambeau <blambeau@gmail.com>
2011-10-17 11:50:53 +0200

    Clean a test

--- spec/repository/git/test_clone.rb
+++ spec/repository/git/test_clone.rb
@@ -2,44 +2,42 @@ require 'spec_helper'
 module WebSync
   describe Repository::Git, "#clone" do
 
-    before(:all){ 
-      FileUtils.rm_rf(origin = Dir.mktmpdir("websync_test_clone_origin"))
-      @origin = Repository::Git.create(origin)
-    }
+    let(:origin){ Fixtures.the_bare_repository }
 
-    context "without a block" do
-
-      subject{ @origin.clone(where) }
-
-      context "on a non-existing directory" do
-        let(:where){ Dir.mktmpdir("websync_test_clone_existing") }
-        before{ FileUtils.rm_rf(where) }
-        specify{
-          subject.should be_a(WorkingDir)
-          subject.exists?(".gitignore").should be_true
-          subject.in_sync?.should be_true
-        }
-      end
-
-      context "on an existing directory" do
-        let(:where){ File.dirname(__FILE__) }
-        specify{
-          lambda{ subject }.should raise_error(ArgumentError)
-        }
-      end
-
+    context "on a non-existing directory" do
+      subject{ origin.clone(where) }
+      let(:where){ 
+        Dir.mktmpdir("websync_test_clone_existing") 
+      }
+      before{ 
+        FileUtils.rm_rf(where) 
+      }
+      specify{
+        subject.should be_a(WorkingDir)
+        subject.exists?(".gitignore").should be_true
+        subject.in_sync?.should be_true
+      }
     end
 
-    context "with a block" do
-      let(:where){ Dir.mktmpdir }
-      before{ FileUtils.rm_rf(where) }
+    context "on an existing directory" do
+      subject{ origin.clone(where) }
+      let(:where){ File.dirname(__FILE__) }
       specify{
-        seen = nil
-        @origin.clone(where){|cl| seen = cl}
-        seen.should be_a(WorkingDir)
-        seen.exists?(".gitignore").should be_true
-        seen.in_sync?.should be_true
+        lambda{ subject }.should raise_error(ArgumentError)
       }
+    end
+
+    specify "with a block" do
+      Dir.mktmpdir do |where|
+        seen = false
+        origin.clone(where){|cl| 
+          seen = true
+          cl.should be_a(WorkingDir)
+          cl.exists?(".gitignore").should be_true
+          cl.in_sync?.should be_true
+        }
+        seen.should be_true
+      end
     end
 
   end
