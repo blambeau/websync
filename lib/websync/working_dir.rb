4ea9d3f6cfccebaddde7cc738ba2041e08a93c24
Bernard Lambeau <blambeau@gmail.com>
2011-12-15 11:33:00 +0100

    Revisited WorkingDir contract

--- lib/websync/working_dir.rb
+++ lib/websync/working_dir.rb
@@ -1,5 +1,8 @@
 require 'grit'
 module WebSync
+  #
+  # A working dir is a local version of a source code repository called "origin".
+  #
   class WorkingDir
 
     # The underlying path
@@ -32,6 +35,79 @@ module WebSync
     def /(file)
       self.path / file
     end
+
+    ###################################################################### State
+
+    # Is the local copy clean?
+    #
+    # @return [Boolean] true if if there are no local uncommited changes on the 
+    # local copy. Uncommited changes include modified, deleted and added files 
+    # (except explicitely ignored ones).
+    def clean?
+      pending_changes.empty?
+    end
+
+    # The inverse of `clean?`
+    def dirty?
+      !clean?
+    end
+
+    # Does the origin have commits that are not shared by the working dir?
+    #
+    # @return [Boolean] true if such commits exist, false otherwise
+    def backward?
+      !unmerged_commits.empty?
+    end
+
+    # Does the working dir have commits that have not been pushed back to the 
+    # origin.
+    #
+    # @return [Boolean] true if such commits exist, false otherwise
+    def forward?
+      !unpushed_commits.empty?
+    end
+
+    # Is the local copy strictly synchronized with the origin?
+    #
+    # @return [Boolean] true iif (clean? and !backward and !forward)
+    def synchronized?
+      (clean? and !backward and !forward)
+    end
+
+    ################################################################## Operation
+
+    # Creates a commit by saving local changes.
+    #
+    # @dom-pre  dirty?
+    # @dom-post clean?
+    # @req-post [for Avoid(AutoPushToOrigin)] is_forward?
+    # @param [String] message a commit description
+    # @return [WorkingDir] self
+    def save(message)
+      self
+    end
+    undef :save
+
+    # Pushes local commits to the origin.
+    #
+    # @dom-pre  forward?
+    # @dom-post !forward
+    # @req-pre  [for Maintain(LinearHistory)] !backward?
+    # @return [WorkingDir] self
+    def push
+      self
+    end
+    undef :push
+
+    # Pull unmerged commits from the origin
+    #
+    # @dom-pre  backward?
+    # @dom-post !backward?
+    # @return [WorkingDir] self
+    def pull
+      self
+    end
+    undef :pull
 
   end # class WorkingDir
 end # module WebSync
